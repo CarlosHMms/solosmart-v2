08903ab4fa4e860147a445992c87a83ec9c6920a
Thiago Henrique Felix Cândido Ribeiro Conceição <116850972+Thiagohfc@users.noreply.github.com>
2024-10-10 20:29:14 -0300

    breeze instalado

--- routes/web.php
+++ routes/web.php
@@ -1,25 +1,9 @@
 <?php
 
-use App\Http\Controllers\UsuariosController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/usuarios', function () {
-
-    return dd('foi');
-});
-/*
 Route::get('/', function () {
-    return view('welcome');
+    return ['Laravel' => app()->version()];
 });
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
-});
-*/
+require __DIR__.'/auth.php';
