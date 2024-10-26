a82d20d1495bd29ad46142a0e405d0edbc1c18a1
Thiago Henrique Felix Cândido Ribeiro Conceição <116850972+Thiagohfc@users.noreply.github.com>
2024-10-26 15:17:00 -0300

    adicionando grafico de pizza na dashboard

--- solosmart_flutter/lib/views/dashborad_view.dart
+++ solosmart_flutter/lib/views/dashborad_view.dart
@@ -1,9 +1,10 @@
 import 'dart:async';
 import 'dart:convert';
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:solosmart_flutter/utils/provider.dart';
-import 'package:solosmart_flutter/services/generateData.dart'; // Certifique-se de ajustar o caminho
+import 'package:solosmart_flutter/services/generateData.dart';
 
 class DashboardView extends StatefulWidget {
   const DashboardView({super.key});
@@ -19,7 +20,6 @@ class _DashboardViewState extends State<DashboardView> {
   @override
   void initState() {
     super.initState();
-    // Inicia o Timer para gerar e atualizar os dados a cada 10 segundos
     _timer = Timer.periodic(const Duration(seconds: 10), (timer) {
       _gerarEDadosAtualizados();
     });
@@ -28,18 +28,15 @@ class _DashboardViewState extends State<DashboardView> {
   Future<void> _gerarEDadosAtualizados() async {
     try {
       final profileProvider = Provider.of<AllProvider>(context, listen: false);
-      final placaId =
-          profileProvider.placaId; // Obtem o ID da placa selecionada
+      final placaId = profileProvider.placaId;
       final token = profileProvider.token;
 
-      // Verifique se placaId e token não são nulos
       if (placaId == null || token == null) {
         print('Erro: placaId ou token não definidos');
-        return; // Retorne se qualquer um deles for nulo
+        return;
       }
 
-      await profileProvider
-          .atualizarDados(placaId); // Atualiza os dados no Provider
+      await profileProvider.atualizarDados(placaId);
     } catch (e) {
       print('Erro ao gerar ou atualizar dados: $e');
     }
@@ -47,7 +44,7 @@ class _DashboardViewState extends State<DashboardView> {
 
   @override
   void dispose() {
-    _timer?.cancel(); // Cancela o Timer ao sair da tela
+    _timer?.cancel();
     super.dispose();
   }
 
@@ -55,6 +52,11 @@ class _DashboardViewState extends State<DashboardView> {
   Widget build(BuildContext context) {
     final dadosProvider = Provider.of<AllProvider>(context);
     final dados = dadosProvider.dados;
+
+    final double umidadeAr =
+        double.tryParse(dados?['umidade_ar']?.toString() ?? '0') ?? 0;
+    final double umidadeSolo =
+        double.tryParse(dados?['umidade_solo']?.toString() ?? '0') ?? 0;
 
     return Scaffold(
       backgroundColor: const Color(0xFFF5F8DE),
@@ -111,6 +113,72 @@ class _DashboardViewState extends State<DashboardView> {
                           ],
                         ),
                         const SizedBox(height: 20),
+                        // Card para Níveis de Umidade
+                        Card(
+                          elevation: 4,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(16),
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.all(16.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                const Text(
+                                  'Níveis de Umidade',
+                                  style: TextStyle(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                const SizedBox(height: 16),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Flexible(
+                                      flex: 2,
+                                      child: SizedBox(
+                                        height:
+                                            200,
+                                        child: PieChart(
+                                          PieChartData(
+                                            sections: _buildPieChartSections(
+                                                umidadeAr, umidadeSolo),
+                                            centerSpaceRadius: 40,
+                                            sectionsSpace: 4,
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                    Flexible(
+                                      flex: 1,
+                                      child: Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: [
+                                          _buildLegendItem(
+                                            color: Colors.redAccent,
+                                            label: 'Baixa Umidade',
+                                          ),
+                                          const SizedBox(height: 10),
+                                          _buildLegendItem(
+                                            color: Colors.yellowAccent,
+                                            label: 'Média Umidade',
+                                          ),
+                                          const SizedBox(height: 10),
+                                          _buildLegendItem(
+                                            color: Colors.greenAccent,
+                                            label: 'Alta Umidade',
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
                       ],
                     ),
                   ),
@@ -120,6 +188,111 @@ class _DashboardViewState extends State<DashboardView> {
           ),
         ],
       ),
+    );
+  }
+
+
+  List<PieChartSectionData> _buildPieChartSections(
+      double umidadeAr, double umidadeSolo) {
+    // Define categorias de umidade para o gráfico de pizza
+    final double baixaUmidade = 30.0;
+    final double mediaUmidade = 70.0;
+
+    // Calculando os valores para as seções do ar
+    double valorBaixaUmidadeAr = umidadeAr < baixaUmidade ? umidadeAr : 0;
+    double valorMediaUmidadeAr =
+        (umidadeAr >= baixaUmidade && umidadeAr <= mediaUmidade)
+            ? umidadeAr
+            : 0;
+    double valorAltaUmidadeAr = umidadeAr > mediaUmidade ? umidadeAr : 0;
+
+    // Calculando os valores para as seções do solo
+    double valorBaixaUmidadeSolo = umidadeSolo < baixaUmidade ? umidadeSolo : 0;
+    double valorMediaUmidadeSolo =
+        (umidadeSolo >= baixaUmidade && umidadeSolo <= mediaUmidade)
+            ? umidadeSolo
+            : 0;
+    double valorAltaUmidadeSolo = umidadeSolo > mediaUmidade ? umidadeSolo : 0;
+
+    return [
+      // Seção de baixa umidade do ar
+      PieChartSectionData(
+        value: valorBaixaUmidadeAr,
+        color: Colors.redAccent,
+        title: valorBaixaUmidadeAr > 0
+            ? 'Ar: ${valorBaixaUmidadeAr.toStringAsFixed(1)}%'
+            : '',
+        radius: 60,
+      ),
+      // Seção de média umidade do ar
+      PieChartSectionData(
+        value: valorMediaUmidadeAr,
+        color: Colors.yellowAccent,
+        title: valorMediaUmidadeAr > 0
+            ? 'Ar: ${valorMediaUmidadeAr.toStringAsFixed(1)}%'
+            : '',
+        radius: 60,
+      ),
+      // Seção de alta umidade do ar
+      PieChartSectionData(
+        value: valorAltaUmidadeAr,
+        color: Colors.greenAccent,
+        title: valorAltaUmidadeAr > 0
+            ? 'Ar: ${valorAltaUmidadeAr.toStringAsFixed(1)}%'
+            : '',
+        radius: 60,
+      ),
+      // Seção de baixa umidade do solo
+      PieChartSectionData(
+        value: valorBaixaUmidadeSolo,
+        color: Colors.redAccent.withOpacity(0.5),
+        title: valorBaixaUmidadeSolo > 0
+            ? 'Solo: ${valorBaixaUmidadeSolo.toStringAsFixed(1)}%'
+            : '',
+        radius: 60,
+      ),
+      // Seção de média umidade do solo
+      PieChartSectionData(
+        value: valorMediaUmidadeSolo,
+        color: Colors.yellowAccent.withOpacity(0.5),
+        title: valorMediaUmidadeSolo > 0
+            ? 'Solo: ${valorMediaUmidadeSolo.toStringAsFixed(1)}%'
+            : '',
+        radius: 60,
+      ),
+      // Seção de alta umidade do solo
+      PieChartSectionData(
+        value: valorAltaUmidadeSolo,
+        color: Colors.greenAccent.withOpacity(0.5),
+        title: valorAltaUmidadeSolo > 0
+            ? 'Solo: ${valorAltaUmidadeSolo.toStringAsFixed(1)}%'
+            : '',
+        radius: 60,
+      ),
+    ];
+  }
+
+
+  Widget _buildLegendItem({required Color color, required String label}) {
+    return Row(
+      children: [
+        Container(
+          width: 16,
+          height: 16,
+          decoration: BoxDecoration(
+            color: color,
+            shape: BoxShape.circle,
+          ),
+        ),
+        const SizedBox(width: 8),
+        Text(
+          label,
+          style: const TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ],
     );
   }
 
