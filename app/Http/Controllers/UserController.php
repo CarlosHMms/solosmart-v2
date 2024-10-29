4819827ae57fa3cac40c48c5dc8273b8ead701f3
Carlos Henrique Mota Martins <carloshmmg2@gmail.com>
2024-10-28 22:03:51 -0300

    add edit service

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,6 +6,7 @@ use App\Http\Resources\UserResource;
 use Illuminate\Http\Request;
 use App\Models\User;
 use App\Traits\HttpResponse;
+use Illuminate\Contracts\Validation\Rule;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
@@ -15,7 +16,7 @@ use Illuminate\Support\Facades\Storage;
 class UserController extends Controller
 {
     use HttpResponse;
-    
+
     public function index()
     {
         return UserResource::collection(User::all());
@@ -28,18 +29,17 @@ class UserController extends Controller
     public function store(Request $request)
     {
         $validator = $this->validation($request);
-        if($validator -> fails()){
+        if ($validator->fails()) {
             return $this->error('Dados inválidos', 400, $validator->errors());
         }
         $validatedData = $validator->validated();
         $validatedData['password'] = Hash::make($validatedData['password']);
 
         $created = User::create($validatedData);
-        if($created){
+        if ($created) {
             return $this->response('Usuário cadastrado', 200, $created);
         }
         return $this->error('Usuário não cadastrado', 401);
-
     }
 
     /**
@@ -52,23 +52,23 @@ class UserController extends Controller
 
     public function getProfile()
     {
-    try {
-        // Obtém o usuário autenticado
-        $user = Auth::user();
+        try {
+            // Obtém o usuário autenticado
+            $user = Auth::user();
 
-        if ($user) {
-            // Log para verificar o usuário autenticado
-            \Log::info('Usuário autenticado: ', ['user_id' => $user->id]);
+            if ($user) {
+                // Log para verificar o usuário autenticado
+                \Log::info('Usuário autenticado: ', ['user_id' => $user->id]);
 
-            // Retorna os dados do usuário autenticado
-            return $this->response('Perfil do usuário recuperado com sucesso', 200, $user);
+                // Retorna os dados do usuário autenticado
+                return $this->response('Perfil do usuário recuperado com sucesso', 200, $user);
+            }
+
+            return $this->error('Usuário não autenticado', 401);
+        } catch (\Exception $e) {
+            \Log::error('Erro ao recuperar o perfil do usuário: ' . $e->getMessage());
+            return $this->error('Erro ao recuperar o perfil do usuário', 500);
         }
-
-        return $this->error('Usuário não autenticado', 401);
-    } catch (\Exception $e) {
-        \Log::error('Erro ao recuperar o perfil do usuário: ' . $e->getMessage());
-        return $this->error('Erro ao recuperar o perfil do usuário', 500);
-    }
     }
 
 
@@ -76,18 +76,46 @@ class UserController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(string $id, Request $request)
     {
-        //
-    }
+        //Valido os dados da requisição
+        $validator = $this->validationEdit($request);
+        if ($validator->fails()) {
+            return $this->error('Validation failed', 422, $validator->errors());
+        }
+        //Verifico se o usuário existe
+        $usuario = User::find($id);
+        if (!$usuario) {
+            return $this->error('Não foi possivel concluir a edição', 404);
+        }
 
+        $validatedData = $validator->validated();
+
+        //Valido e criptografo a senha fornecida caso exista
+        if ($validatedData['password']) {
+            $validatedData['password'] = Hash::make($validatedData['password']);
+
+            //Verifico se a senha antiga é válida
+            if (!Hash::check($validatedData['old_password'], $usuario->password)) {
+                return $this->error('Senha Incorreta', 400);
+            }
+        }
+
+        //Atualizo os dados do usuário
+
+        try {
+            $usuario->update($validatedData);
+        } catch (\Exception $e) {
+            return $this->error('Não foi possivel concluir a edição', 400);
+        }
+    }
     /**
      * Update the specified resource in storage.
      */
     public function update(Request $request, string $id)
     {
         $validator = $this->validation($request);
-        if ($validator->fails()){
+        if ($validator->fails()) {
             return $this->error('Validation failed', 422, $validator->errors());
         }
         $validatedData = $validator->validated();
@@ -98,37 +126,36 @@ class UserController extends Controller
             'password' => $validatedData['password'],
             'nivel_acesso' => $validatedData['nivel_acesso']
         ]);
-        if($updated){
+        if ($updated) {
             return $this->response('usuário atualizado', 200, $request->all());
-
         }
         return $this->error('', 400);
     }
 
     public function updateProfileImage(Request $request)
-{
-    $request->validate([
-        'profile_image' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048',
-    ]);
+    {
+        $request->validate([
+            'profile_image' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048',
+        ]);
 
-    $user = Auth::user();
+        $user = Auth::user();
 
-    if ($request->hasFile('profile_image')) {
-        // Remove a imagem antiga, se existir
-        if ($user->profile_image) {
-            Storage::delete($user->profile_image);
+        if ($request->hasFile('profile_image')) {
+            // Remove a imagem antiga, se existir
+            if ($user->profile_image) {
+                Storage::delete($user->profile_image);
+            }
+
+            // Salva a nova imagem no disco público
+            $path = $request->file('profile_image')->store('profiles', 'public'); // Adicione 'public' aqui
+
+            // Atualiza o caminho da imagem no perfil do usuário
+            $user->profile_image = Storage::url($path); // Gera a URL correta
+            $user->save();
         }
 
-        // Salva a nova imagem no disco público
-        $path = $request->file('profile_image')->store('profiles', 'public'); // Adicione 'public' aqui
-
-        // Atualiza o caminho da imagem no perfil do usuário
-        $user->profile_image = Storage::url($path); // Gera a URL correta
-        $user->save();
+        return response()->json(['message' => 'Imagem de perfil atualizada com sucesso!'], 200);
     }
-
-    return response()->json(['message' => 'Imagem de perfil atualizada com sucesso!'], 200);
-}
 
 
     /**
@@ -137,17 +164,30 @@ class UserController extends Controller
     public function destroy(User $usuario)
     {
         $deleted = $usuario->delete();
-        if ($deleted){
+        if ($deleted) {
             return $this->response('usuário deletado', 200);
         }
         return $this->error('usuário não foi deletado', 400);
     }
 
-    public function validation(Request $request){
+    public function validation(Request $request)
+    {
         $validator = Validator::make($request->all(), [
             'name' => 'required|string|max:100',
             'email' => 'required|string|email|max:255|unique:users,email',
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
+        ]);
+        return $validator;
+    }
+    public function validationEdit(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'string|max:100',
+            'email' => 'string|email|max:255|unique:users,email',
+            'password' =>  'confirmed',
+            Rules\Password::defaults(),
+            'old_password' => "required",
+            Rules\Password::defaults(),
         ]);
         return $validator;
     }
