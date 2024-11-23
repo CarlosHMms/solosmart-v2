2a191fc0f0564a263ecce5f0f5edf22091f7c5ca
Thiago Henrique Felix Cândido Ribeiro Conceição <116850972+Thiagohfc@users.noreply.github.com>
2024-11-23 19:58:01 -0300

    modificando a tela de relatório

--- solosmart_flutter/lib/views/relatorios_view.dart
+++ solosmart_flutter/lib/views/relatorios_view.dart
@@ -1,143 +1,188 @@
 import 'package:flutter/material.dart';
 
-class RelatoriosView extends StatefulWidget {
-  const RelatoriosView({super.key});
-
-  @override
-  State<RelatoriosView> createState() => _RelatoriosViewState();
-}
-
-class _RelatoriosViewState extends State<RelatoriosView> {
-  // Exemplo de dados para a tabela
-  final List<Map<String, String>> _tabelaDados = [
-    {"hora": "08:00", "data": "2024-10-29", "situacao": "Normal"},
-    {"hora": "12:00", "data": "2024-10-29", "situacao": "Alerta"},
-    {"hora": "16:00", "data": "2024-10-29", "situacao": "Crítico"},
-    // Adicione mais dados conforme necessário
-  ];
-
-  final TextEditingController _dataFiltroController = TextEditingController();
-
-  // Função para exibir todos os dados da tabela
-  void _exibirTodosDados() {
-    // Função de lógica para exibir todos os dados (atualmente não filtrado)
-  }
-
-  // Função para baixar a tabela (implementação conforme a necessidade)
-  void _baixarTabela() {
-    // Função de lógica para baixar a tabela (exemplo básico)
-    print("Baixando tabela...");
-  }
+class RelatoriosView extends StatelessWidget {
+  const RelatoriosView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Relatórios'),
-        backgroundColor: const Color(0xFF41337A), // Cor do AppBar
-      ),
-      body: Container(
-        color: const Color(0xFFF5F8DE), // Cor de fundo das outras views
+      backgroundColor: const Color(0xFFF5F8DE),
+      body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
           children: [
-            // Campo de Filtro por Data
             Row(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Expanded(
-                  child: TextField(
-                    controller: _dataFiltroController,
-                    decoration: InputDecoration(
-                      hintText: 'Filtrar por data (AAAA-MM-DD)',
-                      filled: true,
-                      fillColor: const Color(0xFFF5F5F5),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(15),
-                        borderSide: const BorderSide(color: Colors.black),
-                      ),
-                    ),
-                  ),
-                ),
+                _buildDatePicker(context, label: 'Sexta, 13/10/23'),
+                const SizedBox(width: 16),
+                _buildDatePicker(context, label: 'Quinta, 29/11/23'),
                 const SizedBox(width: 16),
                 ElevatedButton(
-                  onPressed: _exibirTodosDados,
+                  onPressed: () {
+                    // Implementar funcionalidade de filtro
+                  },
                   style: ElevatedButton.styleFrom(
                     backgroundColor: const Color(0xFF41337A),
                     padding: const EdgeInsets.symmetric(
-                        horizontal: 16, vertical: 12),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(15),
-                    ),
+                        horizontal: 20, vertical: 10),
                   ),
                   child: const Text(
-                    'Exibir Todos',
-                    style: TextStyle(fontSize: 18, color: Colors.white),
+                    'Filtrar',
+                    style: TextStyle(color: Colors.white),
                   ),
                 ),
               ],
             ),
-            const SizedBox(height: 16),
-            // Tabela de Dados
+            const SizedBox(height: 20),
             Expanded(
               child: SingleChildScrollView(
                 scrollDirection: Axis.horizontal,
-                child: DataTable(
-                  columns: const [
-                    DataColumn(
-                      label: Text(
-                        'Hora',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 18,
-                        ),
+                child: Container(
+                  width: MediaQuery.of(context).size.width * 0.5,
+                  decoration: BoxDecoration(
+                    color: const Color.fromARGB(255, 255, 255, 255), // Cor de fundo
+                    borderRadius:
+                        BorderRadius.circular(16), // Bordas arredondadas
+                    border: Border.all(
+                        color: Colors.grey,
+                        width: 1), // Borda ao redor da tabela
+                  ),
+                  padding: const EdgeInsets.all(8), // Espaçamento interno
+                  child: SingleChildScrollView(
+                    child: Table(
+                      columnWidths: const {
+                        0: FlexColumnWidth(1),
+                        1: FlexColumnWidth(1),
+                        2: FlexColumnWidth(1),
+                      },
+                      border: TableBorder(
+                        horizontalInside: BorderSide(
+                            color: Colors.grey[400]!,
+                            width: 1), // Linhas horizontais
+                        verticalInside: BorderSide(
+                            color: Colors.grey[400]!,
+                            width: 1), // Linhas verticais
                       ),
-                    ),
-                    DataColumn(
-                      label: Text(
-                        'Data',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 18,
+                      children: [
+                        // Cabeçalho da Tabela
+                        TableRow(
+                          decoration: const BoxDecoration(
+                            color: Color(0xFF41337A), // Fundo do cabeçalho
+                          ),
+                          children: const [
+                            Padding(
+                              padding: EdgeInsets.symmetric(vertical: 8),
+                              child: Center(
+                                child: Text(
+                                  'Hora',
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                              ),
+                            ),
+                            Padding(
+                              padding: EdgeInsets.symmetric(vertical: 8),
+                              child: Center(
+                                child: Text(
+                                  'Data',
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                              ),
+                            ),
+                            Padding(
+                              padding: EdgeInsets.symmetric(vertical: 8),
+                              child: Center(
+                                child: Text(
+                                  'Situação',
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                              ),
+                            ),
+                          ],
                         ),
-                      ),
+                        // Linhas da Tabela
+                        ...List.generate(15, (index) {
+                          return TableRow(
+                            decoration: BoxDecoration(
+                              color: index % 2 == 0
+                                  ? Colors.white
+                                  : Colors
+                                      .grey[200], // Cor alternada para linhas
+                            ),
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.all(8),
+                                child: Center(child: Text('Hora ${index + 1}')),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.all(8),
+                                child: Center(child: Text('Data ${index + 1}')),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.all(8),
+                                child: Center(
+                                  child: Text(index % 2 == 0 ? 'Bom' : 'Ruim'),
+                                ),
+                              ),
+                            ],
+                          );
+                        }),
+                      ],
                     ),
-                    DataColumn(
-                      label: Text(
-                        'Situação',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 18,
-                        ),
-                      ),
-                    ),
-                  ],
-                  rows: _tabelaDados.map((dados) {
-                    return DataRow(cells: [
-                      DataCell(Text(dados['hora']!)),
-                      DataCell(Text(dados['data']!)),
-                      DataCell(Text(dados['situacao']!)),
-                    ]);
-                  }).toList(),
+                  ),
                 ),
               ),
             ),
-            const SizedBox(height: 16),
-            // Botão de Baixar Tabela
-            ElevatedButton(
-              onPressed: _baixarTabela,
-              style: ElevatedButton.styleFrom(
-                backgroundColor: const Color(0xFF41337A),
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(15),
-                ),
-              ),
-              child: const Text(
-                'Baixar Tabela',
-                style: TextStyle(fontSize: 18, color: Colors.white),
-              ),
+          ],
+        ),
+      ),
+      floatingActionButton: Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment:
+            MainAxisAlignment.end, // Garante alinhamento ao final
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(bottom: 100), // Ajuste aqui a altura
+            child: FloatingActionButton(
+              heroTag: 'exportButton',
+              onPressed: () {
+                // Implementar funcionalidade de exportar dados
+              },
+              backgroundColor: const Color(0xFF41337A),
+              child: const Icon(Icons.download),
             ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildDatePicker(BuildContext context, {required String label}) {
+    return InkWell(
+      onTap: () {
+        // Implementar seleção de data
+      },
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          border: Border.all(color: const Color(0xFF41337A)),
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Row(
+          children: [
+            Text(
+              label,
+              style: const TextStyle(color: Colors.black),
+            ),
+            const SizedBox(width: 8),
+            const Icon(Icons.calendar_today, color: Color(0xFF41337A)),
           ],
         ),
       ),
