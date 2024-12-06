b4b8cb420544c0af532e7a897aa602dd387d5314
Carlos Henrique Mota Martins <107642033+CarlosHMms@users.noreply.github.com>
2024-12-06 13:12:37 -0300

    create list by date function

--- app/Http/Controllers/GravacaoController.php
+++ app/Http/Controllers/GravacaoController.php
@@ -13,11 +13,24 @@ class GravacaoController extends Controller
     use HttpResponse;
     public function index()
     {
-        if (!auth()->user()->tokenCan("gravacoes-index")){
+        if (!auth()->user()->tokenCan("gravacoes-index")) {
             return $this->error("Unauthorized", 403);
         }
-        
+
         return response()->json(Gravacoes::all());
-        
+    }
+
+    public function listByDate(Request $request)
+    {
+        if (!auth()->user()->tokenCan("gravacoes-index")) {
+            return $this->error("Unauthorized", 403);
+        }
+
+        $initialDate = $request->initialDate;
+        $finalDate = $request->finalDate;
+
+        $gravacoes = Gravacoes::whereInBetween('data', [$initialDate, $finalDate]);
+
+        return $this->response("Gravações encontradas", 200, $gravacoes);
     }
 }
